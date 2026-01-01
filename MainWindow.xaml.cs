af25bcdb11b2b9f5f8cbee78e8c9207644589554
Joseph Sauerland <32148757+jsauerland@users.noreply.github.com>
2026-01-01 13:28:53 -0500

    initial commity
    Initial project creation

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using ClockbusterApps.Services;
+using ClockbusterApps.Views;
+using ClockbusterApps.Services;
+using ClockbusterApps.Views;
+using System.Windows;
+
+namespace ClockbusterApps;
+
+public partial class MainWindow : Window
+{
+    private readonly TimingService _timingService;
+    private bool _isLogging;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _timingService = new TimingService();
+    }
+
+    private void BtnToggleLogging_Click(object sender, RoutedEventArgs e)
+    {
+        if (!_isLogging)
+        {
+            _timingService.Start();
+            BtnToggleLogging.Content = "Stop Logging";
+            StatusText.Text = "Status: Logging Active";
+            _isLogging = true;
+        }
+        else
+        {
+            _timingService.Stop();
+            BtnToggleLogging.Content = "Start Logging";
+            StatusText.Text = "Status: Idle";
+            _isLogging = false;
+        }
+    }
+
+    private void ViewData_Click(object sender, RoutedEventArgs e)
+    {
+        var viewer = new TimeclockViewerWindow();
+        viewer.Owner = this;
+        viewer.Show();
+    }
+
+    private void Exit_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
---
+++ Models/LogEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClockbusterApps.Models
+{
+    internal class LogEntry
+    {
+    }
+}
